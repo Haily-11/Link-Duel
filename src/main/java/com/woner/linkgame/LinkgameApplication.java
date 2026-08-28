f004ccd2133e83afc8fad2d21859d9a2cd9b1de7
Haily <Haily@haily11>
2026-08-28 11:46:57 +0800

    feat: initial commit with maven and base setup

---
+++ src/main/java/com/woner/linkgame/LinkgameApplication.java
@@ -0,0 +1,13 @@
+package com.woner.linkgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woner/linkgame/LinkgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woner.linkgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
